543e8dfd506c8bacbf810d975e4e041e853966c4
kong373 <18162531545@163.com>
2026-08-15 16:06:32 +0800

    fix: prevent recharge product card overflow

--- lib/features/commerce/presentation/commerce_catalog_pages.dart
+++ lib/features/commerce/presentation/commerce_catalog_pages.dart
@@ -185,7 +185,7 @@ class _RechargeCatalogPageState extends State<RechargeCatalogPage> {
                             crossAxisCount: 2,
                             mainAxisSpacing: 10,
                             crossAxisSpacing: 10,
-                            childAspectRatio: 1.45,
+                            childAspectRatio: 1.25,
                           ),
                           itemCount: _products!.length,
                           itemBuilder: (BuildContext context, int index) {
